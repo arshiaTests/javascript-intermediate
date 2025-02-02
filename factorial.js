332b524ff78f010004811e6eb0eb64f350823b1f
unknown <arshia.org79@gmail.com>
2025-02-02 16:36:38 +0330

    Recursion

--- factorial.js
+++ factorial.js
@@ -11,14 +11,12 @@
  */
 
 // Write your solution here
-function factorial(n){
-    let sum = 1
-    for(i = 1; i<=n ;i++ ){
-        sum *=i
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
     }
-    return sum;
+    return n * factorial(n - 1);
 }
-console.log(factorial(5))
-
+console.log(factorial(5)); // 120
 
 module.exports = factorial;
